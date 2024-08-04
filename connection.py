8392a005e62b3d92b5350c33a8f5c253eab9918d
Sw4gn3m1te <woingenau.lvx@web.de>
2024-08-04 21:22:36 +0200

    vol control

--- connection.py
+++ connection.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
 import configparser
 
+import win_control
+
+import time
 
 def get_login_secret(path):
     cfg = configparser.ConfigParser()
@@ -14,24 +17,56 @@ def get_login_secret(path):
 
 
 class MqttClient(mqtt.Client):
-
+    
+    def __init__(self):
+        super().__init__(mqtt.CallbackAPIVersion.VERSION2)
+    
     def on_connect(self, mqttc, obj, flags, reason_code, properties):
         print("rc: " + str(reason_code))
-        self.subscribe("homeassistant/#")
+        self.subscribe("hass/desktop/win_vol")
 
     def on_message(self, mqttc, obj, msg):
-        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+        print(f"{msg.topic}: {msg.payload}")
+        try:
+            if msg.topic.endswith("win_vol"):
+                win_control.set_volume(int(msg.payload))
+        except Exception as e:
+            print(e)
 
-    def run(self, config_file):
+    def start(self, config_file):
         init = get_login_secret(config_file)
         self.username_pw_set(*init[0])
         self.connect(*init[1])
-        self.loop_forever()
+        self.loop_start()
+
+    def stop(self):
+        self.loop_stop()
+        self.disconnect()
 
 
 if __name__ == '__main__':
 
     SECRET_PATH = "./secret.ini"
-    client = MqttClient(mqtt.CallbackAPIVersion.VERSION2).run(SECRET_PATH)
+    client = MqttClient()
+    client.start(SECRET_PATH)
+    sender = MqttClient()
+    sender.start(SECRET_PATH)
+
+    try:
+        while True:
+            msg = input("Enter MSG")
+            try:
+                msg = int(msg)
+            except:
+                continue
+            if msg == 1337:
+                break
+            sender.publish("hass/web/win_vol", msg, 0)
+    except Exception as e:
+        print(f"{e} in connection.py")
+
+    client.stop()
+    sender.stop()
+
 
 
--- win_control.py
+++ win_control.py
@@ -1,27 +1,15 @@
-import ctypes
 from ctypes import POINTER, cast
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-
+import pythoncom
 
 def set_volume(volume_level):
-    """
-    Sets the system volume to the specified level.
 
-    :param volume_level: Volume level (float) between 0.0 and 1.0
-    """
-    if not 0.0 <= volume_level <= 1.0:
-        raise ValueError("Volume level must be between 0.0 and 1.0")
-
+    if not 0 <= volume_level <= 100:
+        return "E"
+    pythoncom.CoInitialize()
     devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(
-        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-    # Set the volume level
-    volume.SetMasterVolumeLevelScalar(volume_level, None)
-
-
-# Example usage:
-# Set the volume to 50%
-set_volume(0.5)
+    volume.SetMasterVolumeLevelScalar(float(volume_level/100), None)
